9fbfbb65763b7b532b83265b166bf86047474979
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-28 19:06:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(0);
-  return array
+  return array.slice(0);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
