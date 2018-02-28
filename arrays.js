dbb2ae9a1aa47ab67aaa7b8938281888cd6bad7e
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-28 18:48:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,16 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array
 }
+
+function addElementToEndOfArray(array, element){
+  return [...aray, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, element){
+  return array[element];
+}
