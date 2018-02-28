78c25ebab3aac229f552bd4ad792373004a8992a
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-28 18:41:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  chocolateBars.unshift(element);
+  return chocolateBars
+}
